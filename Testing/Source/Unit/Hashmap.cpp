ee5242d89ec264d3170f2a7f6889e4d34187b97b
cemkalyoncu <cemkalyoncu@gmail.com>
2026-03-22 18:39:13 +0200

    * Activated hasmap test

--- Testing/Source/Unit/Hashmap.cpp
+++ Testing/Source/Unit/Hashmap.cpp
@@ -41,149 +41,120 @@ public:
 int A::acnt=0;
 //...Iterator Delete, Remove
 //
-//TEST_CASE("Hashmap", "[Hashmap]") {
-//	Hashmap<string, A> amap;
-//	
-//	A a1, a2;
-//	
-//	A &a3 = *new A();
-//	
-//	amap.Add("one", a1);
-//	amap.Add("two", a2);
-//	amap.Add("three", a3);
-//	
-//	REQUIRE(amap.GetCount() == 3);
-//	
-//	amap.Remove("two");
-//	
-//	REQUIRE(amap.GetCount() == 2);
-//	
-//	REQUIRE(A::acnt == 3);
-//	
-//	amap.Delete("three");
-//	
-//	REQUIRE(amap.GetCount() == 1);
-//	REQUIRE(A::acnt == 2);
-//	
-//	amap.RemoveAll();
-//	
-//	REQUIRE(A::acnt == 2);
-//	REQUIRE(amap.GetCount() == 0);
-//}
-//
-//TEST_CASE("Hashmap iterators", "[Hashmap]") {
-//	Hashmap<string, A> amap;
-//	
-//	A a1, a2;
-//	
-//	A &a3 = *new A();
-//	
-//	amap.Add("one", a1);
-//	amap.Add("two", a2);
-//	amap.Add("three", a3); // this should be second item as "three"<"two"
-//	
-//	int i=0;
-//	for(auto it : amap) {
-//		switch(i) {
-//			case 0:
-//				REQUIRE( it.first=="one" );
-//				REQUIRE( &it.second == &a1 );
-//				break;
-//			case 1:
-//				REQUIRE( it.first=="three" );
-//				REQUIRE( &it.second == &a3 );
-//				(amap.begin()+1).SetItem(a1);
-//				REQUIRE( &(amap.begin()+1).Current().second==&a1 );
-//				break;
-//			case 2:
-//				REQUIRE( it.first=="two" );
-//				REQUIRE( &it.second == &a2 );
-//				break;
-//		}
-//		i++;
-//	}
-//}
-//
-//TEST_CASE("Hashmap constructors", "[Hashmap]") {
-//	Hashmap<string, A> amap = { 
-//		{"1", new A(1)}, 
-//		{"2", new A(2)}, 
-//		{"3", new A(3)} 
-//	};
-//	int i=0;
-//	for(auto it : amap) {
-//		switch(i) {
-//			case 0:
-//				REQUIRE( it.first=="1" );
-//				REQUIRE( it.second.a==1 );
-//				break;
-//			case 1:
-//				REQUIRE( it.first=="2" );
-//				REQUIRE( it.second.a==2 );
-//				break;
-//			case 2:
-//				REQUIRE( it.first=="3" );
-//				REQUIRE( it.second.a==3 );
-//				break;
-//		}
-//		i++;
-//	}
-//	
-//	Hashmap<string, A> amap2 = { 
-//		{"1", *new A(1)}, 
-//		{"2", *new A(2)}, 
-//		{"3", *new A(3)} 
-//	};
-//	i=0;
-//	for(auto it : amap2) {
-//		switch(i) {
-//			case 0:
-//				REQUIRE( it.first=="1" );
-//				REQUIRE( it.second.a==1 );
-//				break;
-//			case 1:
-//				REQUIRE( it.first=="2" );
-//				REQUIRE( it.second.a==2 );
-//				break;
-//			case 2:
-//				REQUIRE( it.first=="3" );
-//				REQUIRE( it.second.a==3 );
-//				break;
-//		}
-//		i++;
-//	}
-//	
-//	Hashmap<string, A, &A::ToString> amap3 = {
-//		new A(1),
-//		new A(2),
-//		new A(3)
-//	};
-//	amap3.Add(new A(4));
-//	i=0;
-//	for(auto it : amap3) {
-//		switch(i) {
-//			case 0:
-//				REQUIRE( it.first=="1" );
-//				REQUIRE( it.second.a==1 );
-//				break;
-//			case 1:
-//				REQUIRE( it.first=="2" );
-//				REQUIRE( it.second.a==2 );
-//				break;
-//			case 2:
-//				REQUIRE( it.first=="3" );
-//				REQUIRE( it.second.a==3 );
-//				break;
-//			case 3:
-//				REQUIRE( it.first=="4" );
-//				REQUIRE( it.second.a==4 );
-//		}
-//		i++;
-//	}
-//	
-//	amap.Destroy();
-//	amap2.Destroy();
-//	amap3.Destroy();
-//	
-//
-//}
+TEST_CASE("Hashmap", "[Hashmap]") {
+	Hashmap<string, A> amap;
+	
+	A a1, a2;
+	
+	A &a3 = *new A();
+	
+	amap.Add("one", a1);
+	amap.Add("two", a2);
+	amap.Add("three", a3);
+	
+	REQUIRE(amap.GetCount() == 3);
+	
+	amap.Remove("two");
+	
+	REQUIRE(amap.GetCount() == 2);
+	
+	REQUIRE(A::acnt == 3);
+	
+	amap.Delete("three");
+	
+	REQUIRE(amap.GetCount() == 1);
+	REQUIRE(A::acnt == 2);
+	
+	amap.RemoveAll();
+	
+	REQUIRE(A::acnt == 2);
+	REQUIRE(amap.GetCount() == 0);
+}
+
+TEST_CASE("Hashmap iterators", "[Hashmap]") {
+	Hashmap<string, A> amap;
+	
+	A a1, a2;
+	
+	A &a3 = *new A();
+	
+	amap.Add("one", a1);
+	amap.Add("two", a2);
+	amap.Add("three", a3); // this should be second item as "three"<"two"
+	
+	int i=0;
+	for(auto it : amap) {
+		switch(i) {
+			case 0:
+				REQUIRE( it.first=="one" );
+				REQUIRE( &it.second == &a1 );
+				break;
+			case 1:
+				REQUIRE( it.first=="three" );
+				REQUIRE( &it.second == &a3 );
+				(amap.begin()+1).SetItem(a1);
+				REQUIRE( &(amap.begin()+1).Current().second==&a1 );
+				break;
+			case 2:
+				REQUIRE( it.first=="two" );
+				REQUIRE( &it.second == &a2 );
+				break;
+		}
+		i++;
+	}
+}
+
+TEST_CASE("Hashmap constructors", "[Hashmap]") {
+	Hashmap<string, A> amap = { 
+		{"1", new A(1)}, 
+		{"2", new A(2)}, 
+		{"3", new A(3)} 
+	};
+	int i=0;
+	for(auto it : amap) {
+		switch(i) {
+			case 0:
+				REQUIRE( it.first=="1" );
+				REQUIRE( it.second.a==1 );
+				break;
+			case 1:
+				REQUIRE( it.first=="2" );
+				REQUIRE( it.second.a==2 );
+				break;
+			case 2:
+				REQUIRE( it.first=="3" );
+				REQUIRE( it.second.a==3 );
+				break;
+		}
+		i++;
+	}
+	
+	Hashmap<string, A> amap2 = { 
+		{"1", *new A(1)}, 
+		{"2", *new A(2)}, 
+		{"3", *new A(3)} 
+	};
+	i=0;
+	for(auto it : amap2) {
+		switch(i) {
+			case 0:
+				REQUIRE( it.first=="1" );
+				REQUIRE( it.second.a==1 );
+				break;
+			case 1:
+				REQUIRE( it.first=="2" );
+				REQUIRE( it.second.a==2 );
+				break;
+			case 2:
+				REQUIRE( it.first=="3" );
+				REQUIRE( it.second.a==3 );
+				break;
+		}
+		i++;
+	}
+	
+	amap.Destroy();
+	amap2.Destroy();
+	
+
+}
